b1c797e9a40bdc7b862ad63c944a5e8a3c252e7d
Mystic Kitty <djabranton@gmail.com>
2020-11-12 17:34:36 -0800

    Questions worked.  Init and write.fs function done.

---
+++ dist/README.md
@@ -0,0 +1,32 @@
+#test
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  test
+  ## Table of Contents
+  
+ #Installation
+  
+ #Usage
+  
+ #Contributions
+  
+ #Tests
+  
+ #Licenses
+  
+ #Username
+  
+ #Email
+  ## Installation
+  test
+  ## Usage
+  test
+  ## Contributions
+  test
+  ## Tests
+  undefined
+  ## Licenses 
+  This license is covered by the MIT,GNU GPLv3,UNLICENSE,APACHE LICENSE 2.0 license. 
+  ## Username
+  test
+  ## Email 
+  test
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `#${data.title}
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  ${data.description}
+  ## Table of Contents
+  \n #Installation
+  \n #Usage
+  \n #Contributions
+  \n #Tests
+  \n #Licenses
+  \n #Username
+  \n #Email
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributions
+  ${data.contributing}
+  ## Tests
+  ${data.testing}
+  ## Licenses 
+  This license is covered by the ${data.licenses} license. 
+  ## Username
+  ${data.username}
+  ## Email 
+  ${data.email}
 `;
 }
 
